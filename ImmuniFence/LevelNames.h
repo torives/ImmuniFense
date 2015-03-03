6a0ead0dbd83c60522b4a2d6539cc386d096f808
torives <rotcivyves@gmail.com>
2015-03-03 11:35:15 -0300

    Aparentemente rodando, mas com bugs nos tiros ainda

--- ImmuniFence/LevelNames.h
+++ ImmuniFence/LevelNames.h
@@ -9,10 +9,11 @@
 #ifndef ImmuniFense_LevelNames_h
 #define ImmuniFense_LevelNames_h
 
+//se tirar a numeração, temos problemas de leitura do arquivo de waves em LevelWave.
 typedef NS_ENUM (NSUInteger, LevelName) {
-    LevelOne,
-    LevelTwo,
-    LevelThree,
+    LevelOne = 1,
+    LevelTwo = 2,
+    LevelThree = 3,
 };
 
 #endif
